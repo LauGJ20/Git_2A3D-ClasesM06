bfee9f483e61431391002309f89a001d9097a612
LauGJ <laura.garcia.7e8@itb.cat>
2025-12-09 18:22:36 +0100

    todos

---
+++ Practicas DOM/DOM_EJE1/script1.js
@@ -0,0 +1,5 @@
+// Cambiar el texto del <h1>
+document.querySelector('h1').textContent = "Título actualizado";
+
+// Cambiar el texto del párrafo
+document.querySelector('p').textContent = "Texto actualizado";
